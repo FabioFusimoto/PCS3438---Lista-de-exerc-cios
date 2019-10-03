6c2f02464c08ab5e6e32ea43920ae4a9337266ca
Fábio Fusimoto Pires <fabiofusimoto.pires@hotmail.co>
2019-10-03 08:29:00 -0300

    EX1 finalizado
    Implementação completa do EX1

--- EX1.py
+++ EX1.py
@@ -1,7 +1,9 @@
 import statistics as stat
 import csv
-import numpy as np
+# import numpy as np
 import math
+
+trainingLines = 350  # Ou seja, usando as linhas 0-349
 
 
 def getGaussianParameters(samples):
@@ -9,11 +11,10 @@ def getGaussianParameters(samples):
     return(stat.mean(samples), stat.stdev(samples))
 
 
-def readContent(filename: str, trainingLines: int):
+def readContent(filename: str):
     CSVfile = open(filename, newline='')
     fileReader = csv.reader(CSVfile, delimiter=' ', quotechar='|')
     listOfContents = []
-    i = 1
     firstLine = True  # A primeira linha não precisa ser lida, porque é só o nome dos atributos
     for line in fileReader:
         if firstLine:
@@ -23,9 +24,6 @@ def readContent(filename: str, trainingLines: int):
             for row in line[0].split(','):
                 rowList.append(float(row))
             listOfContents.append(rowList)
-            if(i >= trainingLines):
-                break
-            i += 1
     return listOfContents
 
 
@@ -33,20 +31,104 @@ def calcularDensidadeGaussiana(m, dp, x):  # Média, desvio padrão e valor amos
     return((1/(dp*math.sqrt(2*math.pi))) * math.exp((-1)*((x-m)**2)/(2*dp*dp)))
 
 
-fileContents = readContent('class01.csv', 5)
+# ------------- CONSTRUINDO O AVALIADOR BAYESIANO -------------
+fileContents = readContent('class01.csv')
 # print('-----------Dados-----------\r\n')
 # print(fileContents)
 # Transpondo-se os dados, fica mais fácil de se trabalhar
-fileContents = np.transpose(fileContents)
+# fileContents = np.transpose(fileContents)
 # print('\r\n-----------Dados transpostos-----------\r\n')
 # print(fileContents)
 
 # Calculando as médias e desvios padrão para cada um dos atributos
-medias = []
-desviosPadrao = []
-for i in range(len(fileContents)):
-    media, desvioPadrao = getGaussianParameters(fileContents[i])
-    medias.append(media)
-    desviosPadrao.append(desvioPadrao)
+
+# medias = []
+# desviosPadrao = []
+# for i in range(len(fileContents)):
+#     media, desvioPadrao = getGaussianParameters(fileContents[i])
+#     medias.append(media)
+#     desviosPadrao.append(desvioPadrao)
 # print('\r\nMedias: \r\n' + str(medias) + '\r\n')
 # print('\r\nDesvios padrão: \r\n' + str(desviosPadrao) + '\r\n')
+
+# Definindo as classes target e quais linhas pertencem a quais targets
+targets = {}
+for i in range(trainingLines):
+    if(fileContents[i][len(fileContents[i])-1] not in targets.keys()):
+        targets[int(fileContents[i][len(fileContents[i])-1])] = [i]
+    else:
+        targets[int(fileContents[i][len(fileContents[i])-1])].append(i)
+
+# print('\r\nDicionario final:\r\n')
+# print(targets)
+
+# Calculando os atributos gaussianos para cada valor de target e cada atributo
+
+# Os dicionários serão da forma dict[target] = [v1, v2, v3, ..., v99]
+medias = {}
+desviosPadrao = {}
+for key in targets.keys():
+    # print('Para key = ' + str(key) + ' itera-se sobre as linhas:')
+    # print(targets[key])
+    # print(' ')
+    i = 0
+    mediasPorColuna = []
+    desviosPorColuna = []
+    while i < len(fileContents[0]) - 1:  # iterando nas colunas. O -1 serve para excluir a coluna de targets
+        amostras = []
+        for numLinha in targets[key]:  # iterando nas linhas
+            amostras.append(fileContents[numLinha][i])
+        # print('Para o parâmetro x' + str(i) + ' as amostras são:')
+        # print(amostras)
+        # print(' ')
+        mediaDasAmostras, desvioDasAmostras = getGaussianParameters(amostras)
+        mediasPorColuna.append(mediaDasAmostras)
+        desviosPorColuna.append(desvioDasAmostras)
+        i += 1
+    medias[key] = mediasPorColuna
+    desviosPadrao[key] = desviosPorColuna
+
+# f = open('mediasEDesvios.txt', 'w+')
+# for key in medias.keys():
+#     f.write('\r\nTarget = ' + str(key) + '\r\n')
+#     for i in range(len(medias[key])):
+#         f.write('Parâmetro x' + str(i) + ' >> Média = ' + str(medias[key][i]) + ' Variância = ' + str(desviosPadrao[key][i]) + '\r\n')
+
+# f.close()
+
+# ------------- TESTANDO O AVALIADOR BAYESIANO-------------
+acertosNaBaseDeTreino = 0
+acertosForaDaBaseDeTreino = 0
+total = 0
+# f = open('verossimilhanças.txt', 'w+')
+for linha in range(len(fileContents)):  # len(fileContents)
+    # f.write('\r\nLinha #' + str(linha) + '\r\n')
+    vsPorTarget = {}
+    for target in targets.keys():
+        # f.write('\r\nTestando para target = ' + str(target) + '\r\n')
+        vs = 1  # de verossimilhança
+        i = 0
+        while(i < len(medias[target])):
+            DG = calcularDensidadeGaussiana(m=medias[target][i], dp=desviosPadrao[target][i], x=fileContents[linha][i])
+            # f.write('Verossimilhança para o parâmetro x' + str(i) + ' = ' + str(DG) + '\r\n')
+            vs = vs * DG
+            i += 1
+        # f.write('Verossimilhança total: ' + str(vs) + '\r\n')
+        vsPorTarget[target] = vs
+    # Tomando o valor de maior verossimilhança e comparando com o valor real
+    targetVSMaximo = max(vsPorTarget, key=vsPorTarget.get)
+    # f.write('Target de máxima verossimilhança para a linha ' + str(linha) + ': ' + str(targetVSMaximo) + '\r\n')
+    # f.write('Target amostral: ' + str(int(fileContents[linha][len(fileContents[linha]) - 1])) + '\r\n')
+    if(targetVSMaximo == fileContents[linha][len(fileContents[linha]) - 1]):
+        if(linha < trainingLines - 1):
+            acertosNaBaseDeTreino += 1
+        else:
+            acertosForaDaBaseDeTreino += 1
+    total += 1
+
+print('Taxa de acertos na base de treino: ' + str(acertosNaBaseDeTreino) + '/' + str(trainingLines) + ' (' +
+      str(100*(acertosNaBaseDeTreino/trainingLines)) + '%)')
+print('Taxa de acertos na base de validação: ' + str(acertosForaDaBaseDeTreino) + '/' + str(total - trainingLines) + ' ('
+      + str(100*(acertosForaDaBaseDeTreino/(total - trainingLines))) + '%)')
+print('Taxa de acertos total: ' + str(acertosNaBaseDeTreino + acertosForaDaBaseDeTreino) + '/' + str(total) + ' ('
+      + str(100*((acertosNaBaseDeTreino + acertosForaDaBaseDeTreino)/total)) + '%)')
